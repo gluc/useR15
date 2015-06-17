d43a2971fb91b6df5e7a916414c20fbabc5f3f9e
Christoph Glur <christoph.glur@ipub.ch>
2015-06-17 21:14:08 +0200

    add 4 and 5

---
+++ src/04_traversal.R
@@ -0,0 +1,5 @@
+
+CountPresentations <- function(node) {
+  if ( node$isLeaf ) return (1)
+  lapply(node$children, )
+}
---
+++ src/05_conversion.R
@@ -0,0 +1,12 @@
+#Convert to data.frame, hierarchy
+useR <- read.csv('../useR15/data/data.csv', stringsAsFactors = FALSE)
+
+
+#Convert to list of lists
+
+
+#Convert to JSON
+
+
+
+#Convert to XML
